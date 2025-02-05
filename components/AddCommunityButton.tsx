825a5b9a88c61cd0958e818c922b5f876bbcac26
JaviCeRodriguez <javicerodriguez@gmail.com>
2025-02-05 15:29:10 -0300

    fix: add community btn responsive

--- components/AddCommunityButton.tsx
+++ components/AddCommunityButton.tsx
@@ -1,13 +1,15 @@
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function AddCommunityButton() {
   return (
     <Link href="/add-community">
-      <Button className=" bg-tech-gradient text-gray font-bold py-2 px-4 rounded">
+      <Button className="hidden px-4 py-2 font-semibold text-white rounded bg-tech-gradient sm:block">
         Add New Community
       </Button>
+      <Button className="block px-4 py-2 font-semibold text-white rounded bg-tech-gradient sm:hidden">
+        +
+      </Button>
     </Link>
-  )
+  );
 }
-
