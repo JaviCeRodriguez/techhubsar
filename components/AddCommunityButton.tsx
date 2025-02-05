db2cd58309266866dcc0e9a60471311a59fe5476
JaviCeRodriguez <javicerodriguez@gmail.com>
2025-02-05 15:59:38 -0300

    fix: add community btn on lg & xl breakpoints

--- components/AddCommunityButton.tsx
+++ components/AddCommunityButton.tsx
@@ -4,10 +4,10 @@ import { Button } from "@/components/ui/button";
 export default function AddCommunityButton() {
   return (
     <Link href="/add-community">
-      <Button className="hidden px-4 py-2 font-semibold text-white rounded bg-tech-gradient sm:block">
+      <Button className="hidden px-4 py-2 font-semibold text-white rounded bg-tech-gradient sm:block lg:hidden xl:block">
         Add New Community
       </Button>
-      <Button className="block px-4 py-2 font-semibold text-white rounded bg-tech-gradient sm:hidden">
+      <Button className="block px-4 py-2 font-semibold text-white rounded bg-tech-gradient sm:hidden lg:block xl:hidden">
         +
       </Button>
     </Link>
